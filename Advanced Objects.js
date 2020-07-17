e3eddc80be7202954d68e807e14bfe06ed2bd5d3
Cormac Donohoe <57358706+cormac0108@users.noreply.github.com>
2020-07-17 01:10:24 +0100

    An intro to Advanced Objects in Javascript
    the concepts of context, reference type, and instantiation.

---
+++ Advanced Objects.js
@@ -0,0 +1,38 @@
+
+
+// Advanced Objects - use the videos as a reference. 
+
+// reference type 
+[] === []
+[1] === [1]
+
+// enter all these things in the console 
+var object1 = { value: 10 };
+var object2 = object1;
+var object3 = { value: 10 };
+object1 === object2
+object1 === object3
+object1.value = 15;
+object2.value 
+object3.value
+// objects are reference types in JS. A primitive type is defined by the programming language
+// for example in JS we have undefined "hello" null false etc as primitives
+// a reference type is created by the programmer.  creaing an array is just an object
+// at the end of the day  so you are esentially creating a box that is a data structure
+
+
+// context 
+// context vs scope  - scope is created when it sees {} function ( ) {} example
+
+function b ( ) {
+    let a = 4;
+}
+console.log(a);   
+// that is scope. og a wont work. 
+// context tells you where we are within the object 
+
+
+
+
+// instantiation
+
