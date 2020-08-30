1f246b43b139d90350d35630ff599598a8bfdbf1
Cormac Donohoe <57358706+cormac0108@users.noreply.github.com>
2020-08-30 23:56:27 +0100

    Math random number generator with JS

---
+++ 30thOfAugust.js
@@ -0,0 +1,15 @@
+
+//Example -math random number
+
+function ourRandomRange(ourMin, ourMax) {
+    return Math.floor(Math.random() * (ourMax - ourMin+1)) + ourMin;
+}
+
+ourRandomRange(1, 9);
+
+//only change code below this line
+function randomRange( myMin, myMax) {
+    return Math.floor()
+}
+var myRandom = randomRange(5, 15);
+console.log(myRandom);
